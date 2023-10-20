db8b085217f5a335b98266ef6be61338eaf03f5e
LAURA_MAYA DD <mayadray1@gmail.com>
2023-10-20 13:25:35 +0200

    done

--- C7/src/app/developer/developer.component.ts
+++ C7/src/app/developer/developer.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Developer } from '../models/developer.model';
+import { Skill } from '../models/skill.model';
+import { SkillComponent } from '../skill/skill.component';
 @Component({
   selector: 'app-developer',
   templateUrl: './developer.component.html',
   styleUrls: ['./developer.component.scss']
 })
 export class DeveloperComponent {
+
+  developer: Developer = new Developer(
+    'Doe',
+    'John',
+    30,
+    'Male',
+    'Lorem ipsum dolor sit amet...',
+    [
+      new Skill('Angular', 'angular.png', 'https://angular.io/'),
+      new Skill('TypeScript', 'typescript.png', 'https://www.typescriptlang.org/'),
+      // Ajoutez d'autres compétences
+    ]
+  );
 
 }
---
+++ C7/src/app/models/skill.model.ts
@@ -0,0 +1,6 @@
+export class Skill {
+    constructor(
+        public name: string,
+        public logo: string,
+        public site: string) { }
+}
